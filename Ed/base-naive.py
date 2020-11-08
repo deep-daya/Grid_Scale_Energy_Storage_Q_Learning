b5acacf9f72331fa2019cedea39ba1a8833de991
Ed Xue <45838684+edmondxue@users.noreply.github.com>
2020-11-08 15:07:16 -0800

    ready for testing

--- Ed/base-naive.py
+++ Ed/base-naive.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 from pandas.plotting import register_matplotlib_converters
+
 register_matplotlib_converters()
 
 import datetime
@@ -17,15 +18,17 @@ Generate Cumulative Rewards with no battery use
 
 
 """
+
+
 def make_base_csv(df):
     load = df.gridnopv.to_numpy()
     tariff = df.tariff.to_numpy()
     times = pd.to_datetime(df.local_15min)
 
-    df['R_base'] = np.cumsum(load * tariff)
-    rewards = df['R_base'].to_numpy()
-    
-    df.to_csv(r'base.csv')
+    df["R_base"] = np.cumsum(load * tariff)
+    rewards = df["R_base"].to_numpy()
+
+    df.to_csv(r"base.csv")
     # #plotting
     # fig, ax1 = plt.subplots(1, 1, figsize=(10,6))
     # fig.autofmt_xdate()
@@ -56,6 +59,7 @@ def make_base_csv(df):
     # ax2.set_xlim([datetime.date(2014, 7, 8), datetime.date(2014, 7, 11)])
     # plt.show()
 
+
 """
 Naive TOU Case:
 
@@ -68,6 +72,8 @@ battery charges/discharges at power rating
 
 
 """
+
+
 def make_naiveTOU_csv(df):
     load = df.gridnopv.to_numpy()
     tariff = df.tariff.to_numpy()
@@ -76,81 +82,89 @@ def make_naiveTOU_csv(df):
     min_tariff = min(tariff)
     max_tariff = max(tariff)
 
-    #constants
+    # constants
     BAT_KW = 5  # Rated power of battery, in kW
     BAT_KWH = 14  # Rated energy of battery, in kWh.
     # Note Tesla Powerwall rates their energy at 13.5kWh, but at 100% DoD,
     # but I have also seen that it's actually 14kwh, 13.5kWh usable
-    BAT_KWH_MIN = 0.1*BAT_KWH  # Minimum SOE of battery, 10% of rated
-    BAT_KWH_MAX = 0.9*BAT_KWH  # Maximum SOE of battery, 90% of rated
-    BAT_KWH_INIT = 0.5*BAT_KWH  # Starting SOE of battery, 50% of rated
-    HR_FRAC = 15/60  # Data at 15 minute intervals, which is 0.25 hours. Need for conversion between kW <-> kWh
+    BAT_KWH_MIN = 0.1 * BAT_KWH  # Minimum SOE of battery, 10% of rated
+    BAT_KWH_MAX = 0.9 * BAT_KWH  # Maximum SOE of battery, 90% of rated
+    BAT_KWH_INIT = 0.5 * BAT_KWH  # Starting SOE of battery, 50% of rated
+    HR_FRAC = (
+        15 / 60
+    )  # Data at 15 minute intervals, which is 0.25 hours. Need for conversion between kW <-> kWh
 
     # df['bat_eng'], df['R_naive'], df['bat_charge']
-    
-    #set all battery charges to 0 initially
-    df['bat_charge'] = 0
-    df['bat_discharge'] = 0
-    df['bat_eng'] = 0
-    df['R_naive'] = 0
+
+    # set all battery charges to 0 initially
+    df["bat_charge"] = 0
+    df["bat_discharge"] = 0
+    df["bat_eng"] = 0
+    df["R_naive"] = 0
 
     for index, row in df.iterrows():
 
-        #first row initialize
+        # first row initialize
         if index == 0:
-            df.loc[index, 'bat_eng'], df.loc[index, 'R_naive'] =  BAT_KWH_INIT, 0 
+            df.loc[index, "bat_eng"], df.loc[index, "R_naive"] = BAT_KWH_INIT, 0
         else:
-            df.loc[index, 'bat_eng'], df.loc[index, 'R_naive'] = bat_eng_old, R_old
+            df.loc[index, "bat_eng"], df.loc[index, "R_naive"] = bat_eng_old, R_old
 
-        #when tariff is lowest, charge the battery all you can
-        if row['tariff'] == min_tariff:
-            
-            if df.loc[index,'bat_eng']  < BAT_KWH_MAX:
-                df.loc[index, 'bat_charge'] = min(BAT_KWH_MAX- df.loc[index,'bat_eng'] , HR_FRAC *BAT_KW)
-                df.loc[index,'bat_eng'] += df.loc[index, 'bat_charge']
-        
-        #try to discharge as much as possible at max tariff
-        elif row['tariff'] == max_tariff:
-            
-            if df.loc[index,'bat_eng'] > BAT_KWH_MIN:
-                df.loc[index,'bat_discharge'] = min(df.loc[index,'bat_eng']  - BAT_KWH_MIN, HR_FRAC *BAT_KW)
-                df.loc[index,'bat_eng'] -= df.loc[index,'bat_discharge'] 
+        # when tariff is lowest, charge the battery all you can
+        if row["tariff"] == min_tariff:
 
-        #account for battery charge/discharge in the reward
-        df.loc[index,'R_naive'] += (row['gridnopv']-df.loc[index,'bat_discharge'] + df.loc[index, 'bat_charge']) * row['tariff']
+            if df.loc[index, "bat_eng"] < BAT_KWH_MAX:
+                df.loc[index, "bat_charge"] = min(
+                    BAT_KWH_MAX - df.loc[index, "bat_eng"], HR_FRAC * BAT_KW
+                )
+                df.loc[index, "bat_eng"] += df.loc[index, "bat_charge"]
 
-        #update old values:
-        bat_eng_old, R_old = df.loc[index,'bat_eng'], df.loc[index,'R_naive']
+        # try to discharge as much as possible at max tariff
+        elif row["tariff"] == max_tariff:
 
+            if df.loc[index, "bat_eng"] > BAT_KWH_MIN:
+                df.loc[index, "bat_discharge"] = min(
+                    df.loc[index, "bat_eng"] - BAT_KWH_MIN, HR_FRAC * BAT_KW
+                )
+                df.loc[index, "bat_eng"] -= df.loc[index, "bat_discharge"]
 
-    #to csv
-    df.to_csv('naive.csv')
+        # account for battery charge/discharge in the reward
+        df.loc[index, "R_naive"] += (
+            row["gridnopv"]
+            - df.loc[index, "bat_discharge"]
+            + df.loc[index, "bat_charge"]
+        ) * row["tariff"]
 
-    fig, ax1 = plt.subplots(1, 1, figsize=(10,6))
+        # update old values:
+        bat_eng_old, R_old = df.loc[index, "bat_eng"], df.loc[index, "R_naive"]
+
+    # to csv
+    df.to_csv("naive.csv")
+
+    fig, ax1 = plt.subplots(1, 1, figsize=(10, 6))
     fig.autofmt_xdate()
     plt.gcf().autofmt_xdate()
-    xfmt = mdates.DateFormatter('%m-%d-%y %H:%M')
+    xfmt = mdates.DateFormatter("%m-%d-%y %H:%M")
     ax1.xaxis.set_major_formatter(xfmt)
-    ax1.set_xlabel('Date')
-    ax1.set_ylabel('Power, kW')
-
+    ax1.set_xlabel("Date")
+    ax1.set_ylabel("Power, kW")
 
     # p1 = ax1.plot(times, bat_pow)
 
-    #load
+    # load
     p2 = ax1.plot(times, load)
     # p3 = ax1.plot(times, grd_pow.value)
 
-    p1 = ax1.plot(times, df['bat_charge'].to_numpy())
-    p3 = ax1.plot(times, df['bat_discharge'].to_numpy()) 
+    p1 = ax1.plot(times, df["bat_charge"].to_numpy())
+    p3 = ax1.plot(times, df["bat_discharge"].to_numpy())
     # p4 = ax1.plot(times, df['R_naive'].to_numpy())
 
-    color = 'tab:red'
+    color = "tab:red"
     ax2 = ax1.twinx()
-    ax2.set_ylabel('Energy Price, $/kWh', color=color)
+    ax2.set_ylabel("Energy Price, $/kWh", color=color)
     # p4 = ax2.plot(times, tariff, color=color)
-    ax2.tick_params(axis='y', labelcolor=color)
-    ax2.set_ylim([0,1.1*max(tariff)])
+    ax2.tick_params(axis="y", labelcolor=color)
+    ax2.set_ylim([0, 1.1 * max(tariff)])
     ax2.xaxis.set_major_formatter(xfmt)
 
     # plt.legend((p1[0], p2[0], p3[0], p4[0]), ('Battery Power', 'Load Power', 'Grid Power', 'Tariff Rate'), loc='best')
@@ -160,8 +174,9 @@ def make_naiveTOU_csv(df):
     ax2.set_xlim([datetime.date(2014, 7, 8), datetime.date(2014, 7, 11)])
     plt.show()
 
+
 if __name__ == "__main__":
-    df = pd.read_csv(r'load_tariff.csv')
+    df = pd.read_csv(r"load_tariff.csv")
     # make_base_csv(df)
     make_naiveTOU_csv(df)
 
@@ -173,7 +188,6 @@ if __name__ == "__main__":
 
     # #Plot
 
-
     # fig.autofmt_xdate()
     # plt.gcf().autofmt_xdate()
     # xfmt = mdates.DateFormatter('%m-%d-%y %H:%M')
@@ -183,7 +197,7 @@ if __name__ == "__main__":
     # # p1 = ax1.plot(times, bat_pow)
     # p2 = ax1.plot(times, df1['R_naive'])
     # p3 = ax1.plot(times, df2['R_base'])
-############
+    ############
     # color = 'tab:red'
     # ax2 = ax1.twinx()
     # ax2.set_ylabel('Energy Price, $/kWh', color=color)
--- residential.py
+++ residential.py
@@ -3,226 +3,261 @@ import numpy as np
 
 from collections import defaultdict
 
-class Residential():
-    def __init__(self, df, gamma, state_dict):
+
+class Residential:
+    def __init__(self, df, state_dict):
 
         self.df = df
+
+        self.gamma = .95
+        self.alpha = .2
+        self.epsilon = 0.65
+        self.LMP_Mavg = 0
         
-        # self.states = np.zeros(len(df), 4)  
 
+        self.LMP_bins = state_dict["LMP"]
+        self.Load_bins = state_dict["Load"]
+        self.TOU_bins = state_dict["TOU"]
+        self.SOC_bins = state_dict["SOC"]
 
-        self.gamma = gamma
-
-        # self.Q = np.zeros((len(self.states), len(self.actions)))
-        # self.Q = defaultdict(lambda: np.zeros(len(self.actions))) 
-        
-        # print(self.Q)
-        # self.policy = np.zeros(len(self.states))    
-
-        self.LMP_bins = state_dict['LMP']
-        self.Load_bins = state_dict['Load']
-        self.TOU_bins = state_dict['TOU']
-        # self.TOU_bins = [0.06475, 0.076155, 0.1254975, 0.0844, 0.0868675, 0.089075]
-        # self.SOC_bins = []
-
-
-        self.BAT_KWH_MIN = 0.1*14    # Minimum SOE of battery, 10% of rated
-        self.BAT_KWH_MAX = 0.9*14    # Maximum SOE of battery, 90% of rated
+        self.BAT_KWH_MIN = 0.1 * 14  # Minimum SOE of battery, 10% of rated
+        self.BAT_KWH_MAX = 0.9 * 14  # Maximum SOE of battery, 90% of rated
         self.BAT_KW = 5
-        self.HR_FRAC = 15/60  # Data at 15 minute intervals, which is 0.25 hours. Need for conversion between kW <-> kWh
-        
+        # Data at 15 minute intervals, which is 0.25 hours. Need for conversion between kW <-> kWh
+        self.HR_FRAC = (
+            15 / 60
+        )  
 
         # D means discharge the battery to help the utility, H means hold current battery energy
-        self.actions = [self.LMP_buy_D, self.LMP_sell_D, self.wait_D, self.TOU_buy_D, self.TOU_sell_D, self.LMP_buy_H, self.LMP_sell_H, self.wait_H, self.TOU_buy_H, self.TOU_sell_H]
-        #state parameterized by: LMP, TOU, load
-        self.S = np.zeros([len(self.LMP_bins), len(self.TOU_bins), len(self.Load_bins)])
-        self.Q = np.zeros([len(self.LMP_bins), len(self.TOU_bins), len(self.Load_bins), len(self.actions)])
+        # THIS ORDER MATTERS
+        self.action_map = {
+            0: self.LMP_buy,
+            1: self.LMP_sell,
+            2: self.wait,
+            3: self.TOU_buy,
+            4: self.TOU_discharge,
+        }
 
+        # state parameterized by: LMP, TOU, load
+        self.S = np.zeros(
+            [
+                len(self.LMP_bins),
+                len(self.TOU_bins),
+                len(self.Load_bins),
+                len(self.SOC_bins),
+            ]
+        )
+        self.Q = np.zeros(
+            [
+                len(self.LMP_bins),
+                len(self.TOU_bins),
+                len(self.Load_bins),
+                len(self.SOC_bins),
+                len(self.actions),
+            ]
+        )
 
     def get_allowed_actions(self, state):
+        # [self.LMP_buy, self.LMP_sell, self.wait, self.TOU_buy, self.TOU_discharge]
+
         actions = []
         EnergyDiff = self.BAT_KW * self.HR_FRAC
         loss = -self.BAT_KW * self.HR_FRAC
 
-        #if can discharge and doesn't put you under limit
-        if state['SOC'] - EnergyDiff > self.BAT_KWH_MIN
-        #can buy if it doesn't put you over the limit
-        if state['SOC'] + EnergyDiff < self.BAT_KWH_MAX:
-            actions.append(self.LMP_buy)
-            actions.append(self.TOU_buy)
+        # can buy if it doesn't put you over the limit
+        if state["SOC"] + EnergyDiff < self.BAT_KWH_MAX:
+            # actions.append(self.LMP_buy)
+            # actions.append(self.TOU_buy)
+            actions.append(0)
+            actions.append(3)
 
-        #vice versa for sell
-        if state['SOC'] - EnergyDiff > self.BAT_KWH_MIN:
-            actions.append(self.LMP_sell_H)
-            actions.append(self.TOU_sell_H)
-            actions.append(self.LMP_buy_)
-            actions.append(self.TOU_sell_H)
-        
+        # vice versa for sell
+        if state["SOC"] - EnergyDiff > self.BAT_KWH_MIN:
+            # actions.append(self.LMP_sell)
+            # actions.append(self.TOU_discharge)
+            actions.append(1)
+            actions.append(4)
 
-        
-        actions.append(self.wait)
+        # actions.append(self.wait)
+        actions.append(2)
 
         return actions
 
+    def LMP_buy(self, state):
+        # buy 15 mins of power from LMP
+        # kWh * $/kWh
+        LMP_cost = state["LMP"]
+        energy_change = self.BAT_KW * self.HR_FRAC
 
-    def LMP_buy_D(self, state):
-        #buy 15 mins of power from LMP
-        #kWh * $/kWh
-        LMP_cost = -state['LMP'] 
-        
+        return (self.LMP_Mavg - LMP_cost) * energy_change
 
-        return LMP_cost * (self.BAT_KW * self.HR_FRAC), SOC
+    def LMP_sell(self, state):
+        # sell 15 mins of power to LMP
+        # kWh * $/kWh
+        LMP_comp = state["LMP"]
+        energy_change = self.BAT_KW * self.HR_FRAC
 
+        return (LMP_comp - self.LMP_Mavg) * energy_change
 
-    def LMP_sell_D(self, state):
-        #sell 15 mins of power to LMP
-        #kWh * $/kWh
-        LMP_comp = state['LMP'] 
+    def TOU_buy(self, state):
+        # buy 15 mins of power from TOU
+        # kWh * $/kWh
+        TOU_cost = -state["TOU"]
+        energy_change = self.BAT_KW * self.HR_FRAC
 
-        return LMP_comp * (self.BAT_KW * self.HR_FRAC)
+        return TOU_cost * energy_change
 
-    def TOU_buy_D(self, state):
-        #buy 15 mins of power from TOU
-        #kWh * $/kWh
-        TOU_cost = -state['TOU'] 
+    def TOU_discharge(self, state):
+        # discharge 15 mins of power to TOU, offsetting load
+        # kWh * $/kWh
+        TOU_comp = state["TOU"]
+        energy_change = self.BAT_KW * self.HR_FRAC
 
-        return TOU_cost * (self.BAT_KW * self.HR_FRAC)
+        return TOU_comp * energy_change
 
-
-    def TOU_sell_D(self, state):
-        #sell 15 mins of power to TOU
-        #kWh * $/kWh
-        TOU_comp = state['TOU'] 
-
-        return TOU_comp * (self.BAT_KW * self.HR_FRAC)
-
-    def wait_D(self, state):
-        #do nothing
+    def wait(self, state):
+        # do nothing
         return 0
 
-    def LMP_buy_H(self, state):
-        #buy 15 mins of power from LMP
-        #kWh * $/kWh
-        LMP_cost = -state['LMP'] 
-
-        return LMP_cost * (self.BAT_KW * self.HR_FRAC)
-
-
-    def LMP_sell_H(self, state):
-        #sell 15 mins of power to LMP
-        #kWh * $/kWh
-        LMP_comp = state['LMP'] 
-
-        return LMP_comp * (self.BAT_KW * self.HR_FRAC)
-
-    def TOU_buy_H(self, state):
-        #buy 15 mins of power from TOU
-        #kWh * $/kWh
-        TOU_cost = -state['TOU'] 
-
-        return TOU_cost * (self.BAT_KW * self.HR_FRAC)
-
-
-    def TOU_sell_H(self, state):
-        #sell 15 mins of power to TOU
-        #kWh * $/kWh
-        TOU_comp = state['TOU'] 
-
-        return TOU_comp * (self.BAT_KW * self.HR_FRAC)
-
-    def wait_H(self, state):
-        #do nothing
-        return 0
-
-    def createEpsilonGreedyPolicy(self, Q, epsilon, num_actions): 
-        """ 
-        Creates an epsilon-greedy policy based 
-        on a given Q-function and epsilon. 
-        
-        Returns a function that takes the state 
-        as an input and returns the probabilities 
-        for each action in the form of a numpy array  
-        of length of the action space(set of possible actions). 
+    def createEpsilonGreedyPolicy(self, Q, epsilon, num_actions):
         """
-        def policyFunction(LMP_ind, TOU_ind, Load_ind): 
-    
-            Action_probabilities = np.ones(num_actions, 
-                    dtype = float) * epsilon / num_actions 
-                    
-            best_action = np.argmax(Q[LMP_ind][TOU_ind][Load_ind][:]) 
-            Action_probabilities[best_action] += (1.0 - epsilon) 
-            return Action_probabilities 
-    
-        return policyFunction 
+        Creates an epsilon-greedy policy based
+        on a given Q-function and epsilon.
 
+        Returns a function that takes the state
+        as an input and returns the probabilities
+        for each action in the form of a numpy array
+        of length of the action space(set of possible actions).
+        """
 
+        def policyFunction(state):
+            LMP_ind, TOU_ind, Load_ind, SOC_ind = state
+
+            allowed = self.get_allowed_actions(state)
+            num_allowed = len(allowed)
+            Action_probabilities = [
+                float(epsilon / num_allowed) for i in range(num_actions) if i in allowed else 0.0
+            ]
+            # print(Action_probabilities)
+            # Action_probabilities = np.ones(num_actions,
+            #         dtype = float) * epsilon / num_actions
+
+            best_action = np.argmax(Q[LMP_ind][TOU_ind][Load_ind][SOC_ind][:])
+            # print(best_action)
+            Action_probabilities[best_action] += 1.0 - epsilon
+            return Action_probabilities
+
+        return policyFunction
 
     def Q_learning(self, epsilon):
 
-        # Create an epsilon greedy policy function 
-        # appropriately for environment action space 
-        policy = self.createEpsilonGreedyPolicy(self.Q, epsilon, len(self.actions) * 2) 
-        
-        #starting SOC to be kept track of
-        SOC = .5
+        # Create an epsilon greedy policy function
+        # appropriately for environment action space
+        policy = self.createEpsilonGreedyPolicy(self.Q, self.epsilon, len(self.action_map))
+
+        #initialize
+        SOC_ind = 3
+
         for ind, row in self.df.iterrows():
-            
+            #update
+            self.LMP_Mavg 
+
             LMP_ind = row["binned_LMP"]
-            TOU_ind = row['binned_TOU']
-            Load_ind = row['binned_Load']
+            TOU_ind = row["TOU"]
+            Load_ind = row["binned_Load"]
+            
 
-            #  np.zeros([len(self.LMP_bins), len(self.TOU_bins), len(self.Load_bins), len(self.actions)])
-            self.Q[LMP_ind][TOU_ind][Load_ind]
+            # get probabilities of all actions from current state
+            action_probabilities = policy(state)
 
-            # self.Q[s_ind, a_ind] = self.Q[s_ind, a_ind] + 0.2 * (
-            #     row["r"] + self.gamma * max(self.Q[sp_ind, :]) - self.Q[s_ind, a_ind]
-            # )
-            # self.Q
+            # choose action according to
+            # the probability distribution
+            action = np.random.choice(
+                np.arange(len(action_probabilities)), p=action_probabilities
+            )
 
-            # get probabilities of all actions from current state 
-            action_probabilities = policy(state) 
-   
-            # choose action according to  
-            # the probability distribution 
-            action = np.random.choice(np.arange( 
-                      len(action_probabilities)), 
-                       p = action_probabilities) 
-   
-            # take action and get reward, transit to next state 
-            next_state, reward, done, _ = env.step(action) 
-               
-            # TD Update 
-            best_next_action = np.argmax(Q[next_state])     
-            td_target = reward + discount_factor * Q[next_state][best_next_action] 
-            td_delta = td_target - Q[state][action] 
-            Q[state][action] += alpha * td_delta 
-   
-            # done is True if episode terminated    
-            if done: 
+            # take action and get reward, transit to next state
+            # next_state, reward, done, _ = env.step(action)?
+
+            next_state, reward, done = self.get_next(state, action)
+
+            # reward = action() + residual * state['TOU']
+            # if
+
+            # TD Update
+            LMP_ind_new, TOU_ind_new, Load_ind_new, SOC_ind_new = next_state
+            best_next_action = np.argmax(self.Q[LMP_ind_new][TOU_ind_new][Load_ind_new][SOC_ind_new][:])
+            td_target = reward + self.gamma * Q[LMP_ind_new][TOU_ind_new][Load_ind_new][SOC_ind_new][best_next_action]
+            td_delta = td_target - Q[LMP_ind][TOU_ind][Load_ind][SOC_ind][action]
+            Q[LMP_ind][TOU_ind][Load_ind][SOC_ind][action] += self.alpha * td_delta
+
+            # done is True if episode terminated
+            if done:
                 break
-                   
-            state = next_state 
+
+            # state = next_state
+            SOC_ind = SOC_ind_new
+
+        # print(self.Q)
+        # self.extract_policy()
+
+    def get_next(self, state, action):
+        """
+        Given starting state and action
+
+        return: next state, reward, and boolean (done or not)
+        """
+        # self.action_map = {0: self.LMP_buy, 1: self.LMP_sell, 2: self.wait, 3: self.TOU_buy, 4: self.TOU_discharge}
+        # LMP_ind, TOU_ind, Load_ind, SOC_ind = state
+        # no terminating states in this problem
+        done = False
+
+        reward = 0
+
+        newstate = state.copy()
+        # action can only alter the SOC of the state variables
+
+        # if charged: increase SOC, keep load the same
+        if action in [0, 3]:
+            newstate["SOC_ind"] += 1
+
+        # if discharged TOU: decrease SOC, decrease load
+        if action == 4:
+            newstate["SOC_ind"] -= 1
+            newstate["load"] -= self.BAT_KW
+
+        # if discharged LMP: decrease SOC
+        if action == 1:
+            newstate["SOC_ind"] -= 1
+
+        # if wait do nothing
+        if action == 2:
+            pass
+
+        # reward is based on action + residual of load
+        # load to kWh * TOU rate + action component
+        action_component = self.action_map[action](state)
+        reward = -newstate['load'] * self.HR_FRAC * state['TOU'] + action_component
 
 
-        print(self.Q)
-        self.extract_policy()
+        return newstate, reward, done
 
-    def extract_policy(self):
-        for s in range(self.numStates):
-            if not np.any(self.Q[s]):
-                self.policy[s] = np.random.randint(1, self.numActions + 1)
-            else:
-                self.policy[s] = np.argmax(self.Q[s]) + 1
-                
+    # def extract_policy(self):
+    #     for s in range(self.numStates):
+    #         if not np.any(self.Q[s]):
+    #             self.policy[s] = np.random.randint(1, self.numActions + 1)
+    #         else:
+    #             self.policy[s] = np.argmax(self.Q[s]) + 1
 
 
 def main():
     df = pd.read_csv(r"Discretized_State_Space.csv")
-    state_dict = pd.read_pickle(r'bins_Dict.pkl')
+    state_dict = pd.read_pickle(r"bins_Dict.pkl")
 
     print(df.head())
     # print(df2)
-    a = Residential(df, .95, state_dict)
+    print(state_dict)
+    a = Residential(df, 0.95, state_dict)
     # a = Residential()
     # a.Q_learning()
 
